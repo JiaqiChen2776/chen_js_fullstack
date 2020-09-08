874211615d1c7668b983f213e14213ac03eaa8d7
chenjiaqi <chenjiaqi@shangyoo.com>
2020-09-08 18:27:08 +0800

    webpack init

---
+++ workflow/webpack/test_webpack/home/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <h1>{{title}}</h1>
+      <p class="des">{{des}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: '学习webpack',
+            des: '这个一个测试'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
