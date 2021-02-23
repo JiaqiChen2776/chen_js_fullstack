9e7e037f02b20f3259991e868c21fd3408cb80b0
JiaqiChen2776 <171244529@qq.com>
2021-02-23 18:54:56 +0800

    ts补充

--- ts/index.ts
+++ ts/index.ts
@@ -34,61 +34,68 @@
 // console.log(user)
 
 // console.log(greeter(user))
+// -----------------------------------------------
 
 // 基本类型
-let arr1: number[] = [1, 2, 3]
-let arr2: Array<number> = [4, 5, 6]
-enum Color{ red = 1, green, blue }
-let color: Color = Color.green
-let colorName: string = Color[2]
+// let arr1: number[] = [1, 2, 3]
+// let arr2: Array<number> = [4, 5, 6]
+// enum Color{ red = 1, green, blue }
+// let color: Color = Color.green
+// let colorName: string = Color[2]
 
-let a: any = 4
-a = a.toFixed(2)
-a = 'b'
+// let a: any = 4
+// a = a.toFixed(2)
+// a = 'b'
 
-let b: undefined = undefined
-let c: null = null
+// let b: undefined = undefined
+// let c: null = null
+// -----------------------------------------------
 
 // 类型断言
-function func(val: string | number): number {
-  if ((val as string).length) {
-    return (val as string).length
-  } else {
-    return val.toString().length
-  }
-}
+// function func(val: string | number): number {
+//   if ((val as string).length) {
+//     return (val as string).length
+//   } else {
+//     return val.toString().length
+//   }
+// }
+// -----------------------------------------------
 // 定义联合类型
-type Combinable = string | number
-function func3(val: Combinable): number {
-  if ((val as string).length) {
-    return (val as string).length
-  } else {
-    return val.toString().length
-  }
-}
-console.log(func('123'), func3('123'))
+// type Combinable = string | number
+// function func3(val: Combinable): number {
+//   if ((val as string).length) {
+//     return (val as string).length
+//   } else {
+//     return val.toString().length
+//   }
+// }
+// console.log(func('123'), func3('123'))
+// -----------------------------------------------
 
 // void
-function func2() {
-  console.log('void')
-  return null
-}
-let d: void = func2()
+// function func2() {
+//   console.log('void')
+//   return null
+// }
+// let d: void = func2()
+// -----------------------------------------------
 
 // 函数
-function getInfo(name: string, age?: number, hometown: string = '北京'): string {
-  let ageStr: string = age ? `I'm ${age} years old, ` : ''
-  return `I am ${name}, ${ageStr}I come from ${hometown}`
-}
-let introduce: string = getInfo('张三')
+// function getInfo(name: string, age?: number, hometown: string = '北京'): string {
+//   let ageStr: string = age ? `I'm ${age} years old, ` : ''
+//   return `I am ${name}, ${ageStr}I come from ${hometown}`
+// }
+// let introduce: string = getInfo('张三')
+// -----------------------------------------------
 
 // 剩余参数
-function getSum(num1: number, num2: number, ...arr: number[]): number {
-  let sum: number = num1 + num2
-  arr.forEach(i => sum += i )
-  return sum
-}
-let sum = getSum(1, 2, 3, 4, 5)
+// function getSum(num1: number, num2: number, ...arr: number[]): number {
+//   let sum: number = num1 + num2
+//   arr.forEach(i => sum += i )
+//   return sum
+// }
+// let sum = getSum(1, 2, 3, 4, 5)
+// -----------------------------------------------
 
 // 重载
 // function add(a: Combinable, b: Combinable) {
@@ -97,18 +104,173 @@ let sum = getSum(1, 2, 3, 4, 5)
 //   }
 //   return a + b;
 // }
-function add(a: number, b: number): number;
-function add(a: string, b: string): string;
-function add(a: string, b: number): string;
-function add(a: number, b: string): string;
-function add(a: Combinable, b: Combinable) {
-  if (typeof a === 'string' || typeof b === 'string') {
-    return a.toString() + b.toString();
-  }
-  return a + b;
+// function add(a: number, b: number): number;
+// function add(a: string, b: string): string;
+// function add(a: string, b: number): string;
+// function add(a: number, b: string): string;
+// function add(a: Combinable, b: Combinable) {
+//   if (typeof a === 'string' || typeof b === 'string') {
+//     return a.toString() + b.toString();
+//   }
+//   return a + b;
+// }
+// const result = add(12, '3')
+// console.log(result, result.split(' '))
+// -----------------------------------------------
+
+// 接口与重载
+// interface BasketballPlayer {
+//   (name: 'YaoMing', age: number): void;
+//   (name: 'Kobe', age?: number): void;
+//   (name: 'James', age?: number): void;
+// }
+// let playBasketball: BasketballPlayer = function (name, age) {
+//   if (name === 'YaoMing' && age && age >= 25) {
+//     console.log('good Center')
+//   } else if (name === 'Kobe') {
+//       console.log('good guard')
+//   } else if (name === 'Jams') {
+//       console.log('good forward')
+//   } else {
+//       console.log('ordinary baskerball player')
+//   }
+// }
+
+// playBasketball('Kobe', 20)
+// -----------------------------------------------
+
+// interface addVal {
+//   (a: number, b: number): number;
+//   (a: string, b: string): string;
+//   (a: string, b: number): string;
+//   (a: number, b: string): string;
+// }
+// let addFunc: addVal = function (a, b) {
+//   if (typeof a === 'string' || typeof b === 'string') {
+//     return a.toString() + b.toString();
+//   }
+//   return a + b;
+// }
+// const res = addFunc('1', 2)
+// res.split(' ')
+// -----------------------------------------------
+
+// function add({ one, two}) {
+//   return one + two
+// }
+// const total = add({ one: 1, two: 2 })
+// console.log(total)
+// -----------------------------------------------
+
+// let arr: [string, number, boolean]
+// arr = ['a', 1, true]
+// arr.push('b')
+// console.log(arr, arr.length)
+// -----------------------------------------------
+
+// 接口实现
+// interface Girl {
+//   name: string;
+//   age: number;
+//   waistline?: number;
+//   say(): string;
+// }
+
+// class XiaoJieJie implements Girl {
+//   name: string;
+//   age: number;
+//   say() {
+//     return "欢迎光临";
+//   }
+
+//   constructor(name: string, age: number) {
+//     this.name = name
+//     this.age = age
+//   }
+// }
+
+// let girl1 = new XiaoJieJie('chen', 18)
+// console.log(girl1)
+// -----------------------------------------------
+
+
+// 类的继承
+// class Lady {
+//   protected content = 'Hello!'
+//   sayHello() {
+//     return this.content
+//   }
+// }
+
+// class Girl extends Lady {
+//   sayLove() {
+//     return 'I love you'
+//   }
+//   // 类方法的重写
+//   sayHello() {
+//     console.log('1', this.content)
+//     return super.sayHello() + ', my darling!'
+//   }
+// }
+
+// class Test extends Girl {
+//   sayHello() {
+//     console.log('2', this.content)
+//     return super.sayHello()
+//   }
+// }
+
+// let chen = new Test()
+// // console.log(chen.content)
+// console.log(chen.sayHello())
+// -----------------------------------------------
+
+// 私有变量的操作
+// class Girl {
+//   public readonly name: string
+//   content = 'hahahha'
+//   constructor (private _age: number, name: string) {
+//     this.name = name
+//   }
+//   get age() {
+//     return this._age
+//   }
+//   set age(age: number) {
+//     this._age = age
+//   }
+//   static sayHello() {
+//     return 'hi'
+//   }
+// }
+// let chen = new Girl(18, 'chen')
+// console.log('before: ', chen.age)
+// chen.age = 16
+// console.log('after: ', chen.age)
+// console.log(chen.name)
+// // chen.name = 'chenjiaqi'
+
+// // static
+// Girl.sayHello()
+// -----------------------------------------------
+
+
+// 抽象类
+abstract class Developer {
+  abstract skill()
 }
-const result = add(12, '3')
-console.log(result, result.split(' '))
+class WebDeveloper extends Developer {
+  skill() {
+    return ['js', 'html', 'css', 'vue']
+  }
+}
+class DatabaseDeveloper extends Developer {
+  skill() {
+    return ['mysql', 'redis', 'linux']
+  }
+}
 
-
+let chen = new WebDeveloper()
+let wu = new DatabaseDeveloper()
+console.log(chen.skill())
+console.log(wu.skill())
 
--- ts/readme.md
+++ ts/readme.md
@@ -19,29 +19,6 @@ TypeScript是JavaScript的超集
 - boolean
 - number
 - string
-- 数组
-```typescript
-let arr1: number[] = [1, 2, 3]
-let arr2: Array<number> = [1, 2, 3]
-```
-
-- 元组 Tuple
-已知元素数量及类型的数组，访问越界元素时，会使用联合类型(string | number)
-```typescript
-let arr: [string, number]
-x = ['a', 1]
-x[3] = 'b'
-x[4] = true // Error，true不是联合类型
-```
-
-- 枚举 enum
-```typescript
-enum Color{ red = 1, green, blue }
-let color: Color = Color.green    // 2
-let colorName: string = Color[2]  // green
-```
-注：没有设置值时，默认为下标；上一个值为数字时，当前值默认为上一值+1；上一值为非数字时，必须设置当前值。
-
 - Any
 可移除类型检查
 ```typescript
@@ -64,6 +41,59 @@ let c: null = undefined
 
 - Never
 表示永不存在的值的类型，如抛出的异常、无返回值的表达式（如调用不会结束的函数）、箭头函数表达式返回值的类型。所有类型的子类型，只有其本身才能赋值给它。
+
+### 对象类型
+- 数组
+```typescript
+let arr1: number[] = [1, 2, 3]
+let arr2: Array<number> = [1, 2, 3]
+
+// 多种类型数组
+let arr3: (number | string)[] = [1, 'a', 2]
+
+// 数组中对象类型的定义
+let arr4: { name: string, age: number }[] = [
+  { name: '张三', age: 18 }
+]
+// 可写为如下
+// 1. 类型别名
+type Person = { name: string, age: number }
+let arr5: Person[] = [
+  { name: '张三', age: 18 }
+]
+// 2. 类
+class Man = { name: string, age: number }
+let arr6: Man[] = [
+  { name: '张三', age: 18 }
+]
+```
+
+- 元组 Tuple
+已知元素数量及类型的数组，访问越界元素时，会使用联合类型(string | number)
+```typescript
+let arr: [string, number]
+x = ['a', 1]
+x[3] = 'b'
+x[4] = true // Error，true不是联合类型
+```
+注：
+1) `Typescript2.7`后，不可再越界访问了，push后的越界元素也无法通过下标获取，但编译成js后还是可以正常运行的。
+2) 元组与数组的区别：元组元素类型位置固定，不可越界访问；元组还可用于处理`csv`文件内数据(分号分隔每行，逗号分隔元素)。
+```typescript
+// 元组类型的数组
+const school: [string, string, number][] = [
+  ["张三", "学生", 18],
+  ["罗翔", "教师", 28]
+]
+```
+
+- 枚举 enum
+```typescript
+enum Color{ red = 1, green, blue }
+let color: Color = Color.green    // 2
+let colorName: string = Color[2]  // green
+```
+注：没有设置值时，默认为下标；上一个值为数字时，当前值默认为上一值+1；上一值为非数字时，必须设置当前值。
 
 - Object
 非原始类型
@@ -97,12 +127,19 @@ function func(val: Combinable): number {
 }
 ```
 
+- 函数类型
+```typescript
+const zhangSan: () => string = () => {
+  return '张三'
+}
+```
+
 ### 接口
-接口可定义数据的结构及类型
+1. 接口可定义数据的结构及类型，参数可为函数类型，参数可选时加`?`
 ```typescript
 interface Person {
   name: string;
-  age: number;
+  age?: number;
 }
 function greeter(person: Person) {
   return `I am ${person.name}, I'm ${person.age} years old.`
@@ -111,7 +148,71 @@ let user = { name: '张三', age: 18 }
 console.log(greeter(user))
 ```
 
+2. 可往接口中加入任意值
+```typescript
+interface Person {
+  name: string;
+  age?: number;
+  [propname: string]: any;
+}
+const girl = {
+  name: '仙女',
+  age: 18,
+  height: 168 // 任意值
+}
+```
+
+3. 接口和类型别名的区别：接口必须代表对象，类型别名可直接给类型
+```typescript
+// 类型别名
+type GirlType = string;
+// 接口
+interface Girl {
+  name: string,
+  age: number
+}
+```
+
+4. 接口的实现
+接口描述了类的公共部分
+```typescript
+interface Person {
+  name: string;
+  age?: number;
+  say(): string
+}
+class XianNv implements Girl {
+  name = '七仙女'
+  age = 18
+  say() {
+    return `I am ${this.name}, I'm ${age} years old.`
+  }
+}
+```
+
+5. 接口的继承
+一个接口可继承多个接口（合成接口）
+```typescript
+interface Person {
+  name: string,
+  age?: number
+}
+interface Girl {
+  hair: string
+}
+interface Teacher extends Person, Girl {
+  subject: string
+}
+
+let chen = <Teacher>{}
+chen.name = 'chenjiaqi'
+chen.hair = 'short'
+chen.subject = 'math'
+```
+
+
 ### 类
+1. 创造类
 在构造函数的参数上使用`public`等同于创建了同名的成员变量
 ```typescript
 class Student {
@@ -133,6 +234,123 @@ function greeter(person: Person) {
 let user = new Student("张三", 18)  
 console.log(greeter(user))
 ```
+
+2. 类的继承
+类可继承另一个类的属性与方法，子类可重写父类方法
+```typescript
+class Lady {
+  content = 'Hello!'
+  sayHello() {
+    return this.content
+  }
+}
+class Girl extends Lady {
+  sayLove() {
+    return 'I love you'
+  }
+  // 类方法的重写
+  sayHello() {
+    return 'Hi, my darling!'
+  }
+}
+let chen = new Girl()
+console.log(chen.content)   // Hello!
+console.log(chen.sayHello())  // Hi, my darling!
+```
+
+3. super关键字
+- super可调用父类的方法
+```typescript
+class Girl extends Lady {
+  // 类方法的重写
+  sayHello() {
+    return super.sayHello() + ', my darling!'
+  }
+}
+```
+
+- 子类若有构造函数，则必须显式调用父类构造函数，即`super()`，父类无构造函数也要调用`super()`
+```typescript
+class Person {
+  constructor(public name: string) {}
+}
+class Teacher extends Person {
+  constructor(public age: number) {
+    super('chen')
+  }
+}
+```
+
+4. 类的访问类型
+- `public`：无访问限制
+- `private`：只能在本类中访问
+- `protected`：只能在本类及继承的子类中访问（多重继承亦可）
+
+5. Getter、Setter
+私有变量的获取与赋值
+```typescript
+class Girl {
+  constructor (private _age: number) {}
+  get age() {
+    return this._age
+  }
+  set age(age: number) {
+    this._age = age
+  }
+}
+let chen = new Girl(18)
+console.log(chen.age)   // 18
+chen.age = 16
+console.log(chen.age)   // 16
+```
+
+6. 类中的`static`
+若类中的方法被声明为`static`，则可不需要new对象，可直接调用。
+`static`方法中不可引用类中的成员变量。
+```typescript
+class Girl {
+  static sayHello() {
+    return 'Hi'
+  }
+}
+Girl.sayHello() // Hi
+```
+
+7. `readonly`只读属性
+成员变量赋初值后不可更改
+```typescript
+class Girl {
+  public readonly name: string
+  constructor (name: string) {
+    this.name = name
+  }
+}
+let chen = new Girl('chen')
+chen.name = 'chenjiaqi' // Error
+```
+
+8. 抽象类
+抽象类就是每个继承它的类都必须实现其中的抽象方法，抽象类及其抽象方法都用`abtract`开头。
+```typescript
+abstract class Developer {
+  abstract skill()
+}
+class WebDeveloper extends Developer {
+  skill() {
+    return ['js', 'html', 'css', 'vue']
+  }
+}
+class DatabaseDeveloper extends Developer {
+  skill() {
+    return ['mysql', 'redis', 'linux']
+  }
+}
+let chen = new WebDeveloper()
+let wu = new DatabaseDeveloper()
+console.log(chen.skill())
+console.log(wu.skill())
+```
+
 
 ### 函数
 #### 声明及传参
@@ -198,7 +416,27 @@ createEvent(eventInterface: "TouchEvent"): TouchEvent; // 特定重载签名
 createEvent(eventInterface: string): Event; // 非特定重载签名
 ```
 
+5. 使用接口搭配重载签名或非重载签名
+```typescript
+interface addVal {
+  (a: number, b: number): number;
+  (a: string, b: string): string;
+  (a: string, b: number): string;
+  (a: number, b: string): string;
+}
+let addFunc: addVal = function (a, b) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+const res = addFunc('1', 2)
+res.split(' ')
+```
 
+参考：
+- [Typescript 函数重载](http://www.semlinker.com/ts-function-overload/)
+- [Typescript 常见的几种函数重载方法详解与应用示例](https://blog.csdn.net/weixin_30681615/article/details/96225259)
 
 
 
